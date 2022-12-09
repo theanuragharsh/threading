a0712420ba0709585d90dd04dead63467d62d5e1
theanuragharsh <nrgharsh@gmail.com>
2022-12-09 12:58:31 +0530

    making buyProduct feature synchronizable

--- src/main/java/com/threadingbasics/resource/SharedProductResource.java
+++ src/main/java/com/threadingbasics/resource/SharedProductResource.java
@@ -14,7 +14,7 @@ public class SharedProductResource {
         products.put("Ruler Scales", 10);
     }
 
-    public String buyProduct(String key) {
+    public synchronized String buyProduct(String key) {
         if (products.containsKey(key)) {
             Integer quantity = products.get(key);
             if (!products.get(key).equals(0)) {
