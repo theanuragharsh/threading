c6bdd6a9f8a3b6947ff1ebb72427898f12e7987e
theanuragharsh <nrgharsh@gmail.com>
2022-12-09 12:39:10 +0530

    Initial commit

---
+++ src/main/java/com/threadingbasics/resource/SharedProductResource.java
@@ -0,0 +1,17 @@
+package com.threadingbasics.resource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SharedProductResource {
+
+    private Map<String, Integer> products = new HashMap<String, Integer>();
+
+    public SharedProductResource() {
+        products.put("Pen", 10);
+        products.put("Pencil", 50);
+        products.put("Notebook", 150);
+        products.put("Ruler Scales", 10);
+    }
+
+}
---
+++ src/test/java/com/threadingbasics/ThreadingbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threadingbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadingbasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
